c005d484999dac9c54fd97feced89d3d2e71b6da
chandelyash2 <chandel.yash2@gmail.com>
2023-10-13 13:54:43 +0530

    add redesign the homepage

--- src/components/Home/Description.tsx
+++ src/components/Home/Description.tsx
@@ -3,17 +3,16 @@ import Image from "next/image";
 import { Container } from "../common/Container";
 export const Description = ({ img }: { img: string }) => {
   return (
-      <Container>
-        <Image
-          // loader={() => myLoader(img)}
-          // src={`${process.env.NEXT_PUBLIC_API_URL_FILE}${img}`}
-          src='/img/about.png'
-          width={500}
-          height={500}
-          alt="banner"
-          className="w-full"
-        />
-      </Container>
-
+    <Container>
+      <Image
+        // loader={() => myLoader(img)}
+        // src={`${process.env.NEXT_PUBLIC_API_URL_FILE}${img}`}
+        src="/img/about.png"
+        width={500}
+        height={500}
+        alt="banner"
+        className="w-full"
+      />
+    </Container>
   );
 };
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -46,12 +46,12 @@ export const Home = () => {
   }, []);
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col">
       <div
           className="h-[400px] md:h-[800px]  lg:h-screen bg-no-repeat bg-cover"
         style={{ backgroundImage: `url(${img})` }}
       >
-        <h1 className="relative top-[40%] left-10 lg:left-24 text-4xl md:text-6xl font-semibold leading-[60px] md:leading-[82px]">
+        <h1 className="relative top-[40%] left-10 lg:left-24 text-4xl md:text-6xl font-semibold leading-[50px] md:leading-[82px]">
           <span>{text.slice(0, 9)}</span>
           <br />
           <span className="font-bold text-4xl md:text-6xl text-[#ebcc60eb]">
