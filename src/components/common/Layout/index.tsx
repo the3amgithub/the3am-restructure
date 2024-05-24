d9202a57d31661ffc3e64225306ebfd475757a92
Yash Chandel <yashchandel@Yashs-MacBook-Air.local>
2024-05-24 13:48:25 +0530

    change homepage layout

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { twMerge } from "tailwind-merge";
 import { motion, useAnimation } from "framer-motion";
+import { Timer } from "../Timer";
 
 interface LayoutProp {
   children: React.ReactNode;
@@ -40,34 +41,44 @@ export const Layout = ({ children }: LayoutProp) => {
   useEffect(() => {
     let threshold = 1200; // default threshold
 
-    if (screenWidth < 640) { // small screens
+    if (screenWidth < 640) {
+      // small screens
       threshold = 600;
-    } else if (screenWidth < 1024) { // medium screens
+    } else if (screenWidth < 1024) {
+      // medium screens
       threshold = 900;
     } // larger screens use the default threshold
 
     if (scrollPosition < threshold) {
-      controls.start({ 
-        backgroundImage: "linear-gradient(to right top, #000000, #150e12, #20181f, #29212e, #2e2c3f, #2c3247, #27394e, #224054, #1d4251, #1d434d, #204448, #254543)", 
-        transition: { duration: 0.4, ease: "easeInOut" }
+      controls.start({
+        backgroundImage:
+          "linear-gradient(to right top, #000000, #150e12, #20181f, #29212e, #2e2c3f, #2c3247, #27394e, #224054, #1d4251, #1d434d, #204448, #254543)",
+        transition: { duration: 0.4, ease: "easeInOut" },
       });
     } else {
-      controls.start({ 
-        backgroundImage: "linear-gradient(to right top, #254543, #1e3d40, #19343c, #172c36, #16242f, #16242f, #16242f, #16242f, #172c36, #19343c, #1e3d40, #254543)", 
-        transition: { duration: 0.4, ease: "easeInOut" }
+      controls.start({
+        backgroundImage:
+          "linear-gradient(to right top, #254543, #1e3d40, #19343c, #172c36, #16242f, #16242f, #16242f, #16242f, #172c36, #19343c, #1e3d40, #254543)",
+        transition: { duration: 0.4, ease: "easeInOut" },
       });
     }
   }, [scrollPosition, screenWidth, controls]);
 
   return (
     <motion.div
-      initial={{ backgroundImage: "linear-gradient(to right top, #000000, #150e12, #20181f, #29212e, #2e2c3f, #2c3247, #27394e, #224054, #1d4251, #1d434d, #204448, #254543)" }}
+      initial={{
+        backgroundImage:
+          "linear-gradient(to right top, #000000, #150e12, #20181f, #29212e, #2e2c3f, #2c3247, #27394e, #224054, #1d4251, #1d434d, #204448, #254543)",
+      }}
       animate={controls}
       className={twMerge(
         "min-h-[100vh] overflow-x-hidden flex flex-col justify-between"
       )}
     >
-      <Header />
+      <div className="fixed z-[999] w-full">
+        <Timer />
+        <Header />
+      </div>
       <div>{children}</div>
       <Footer />
     </motion.div>
